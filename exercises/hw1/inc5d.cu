068af6f2df722100ba1f4874d4c762d03c617de4
Sten Heinze <sten.heinze@gmail.com>
2025-12-25 18:37:23 +0000

    Fix increment example

--- exercises/hw1/inc5d.cu
+++ exercises/hw1/inc5d.cu
@@ -38,25 +38,29 @@ void vec_print(int *v, const unsigned n) {
   printf("]\n");    
 }
 
-const int N = 4096;
-const int THREADS_PER_BLOCK = 128;  // CUDA maximum is 1024
+const int N = 32;
+const int THREADS_PER_BLOCK = 32;  // CUDA maximum is 1024
 
 // vector add kernel: C = A + B
-__global__ void inc5d(const int *tensor5d, int n) {
+__global__ void inc5d(int *tensor5d, int n) {
   int total_workers = gridDim.x * blockDim.x;
   int dim_size = (int)pow((double)total_workers, 0.2);
 
   // create dimensions
-  int global_idx = threadIdx.x + blockIdx.x * blockDim.xl;
-  int i1 = global_idx % 8;
-  int i2 = global_idx / 8 % 8;
-  int i3 = global_idx / 8 / 8 % 8;
-  int i4 = global_idx / 8 / 8 / 8 % 8;
-  int i5 = global_idx / 8 / 8 / 8 / 8 % 8;
-  printf("(%d, %d, %d, %d, %d)\n", i1, i2, i3, i4, i5);
+  int global_idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int i1 = global_idx % dim_size;
+  int i2 = global_idx / dim_size % dim_size;
+  int i3 = global_idx / dim_size / dim_size % dim_size;
+  int i4 = global_idx / dim_size / dim_size / dim_size % dim_size;
+  int i5 = global_idx / dim_size / dim_size / dim_size / dim_size % dim_size;
 
-  if (i1 < dim_size && i2 < dim_size && i3 < dim_size && i4 < dim_size && i5 < dim_size)
-    tensor5d[i5 * 4096 + i4 * 512 + i3 * 64 + i2 * 8 + i1] += 1;
+  if (i1 < dim_size && i2 < dim_size && i3 < dim_size && i4 < dim_size && i5 < dim_size) {
+    tensor5d[i5 * (int)pow(dim_size, 4) + i4 * (int)pow(dim_size, 3) + i3 * (int)pow(dim_size, 2) + i2 * dim_size + i1] += 1;
+    printf("(%d, %d, %d, %d, %d)\n", i1, i2, i3, i4, i5);
+  }
+  else {
+    printf("(%d, %d, %d, %d, %d) SKIPPED\n", i1, i2, i3, i4, i5);
+  }
 }
 
 int main(){
@@ -86,7 +90,7 @@ int main(){
   cudaDeviceSynchronize();
 
   // copy vector C from device to host:
-  cudaMemcpy(c, d_c, memsize, cudaMemcpyDeviceToHost);
+  cudaMemcpy(a, d_a, memsize, cudaMemcpyDeviceToHost);
   cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
 
   printf("a=");

5ec6aa2b7024435c2a617b374aae44ddb56885be
Sten Heinze <sten.heinze@gmail.com>
2025-12-31 18:47:52 +0000

    More nsight packages to install

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt install nsight-compute
-sudo apt install libboost-program-options-dev
+sudo apt -y install nsight-compute
+sudo apt -y install nsight-systems
+sudo apt -y install libboost-program-options-dev
